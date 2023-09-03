1c8ffe53fc2ae007fbe99b71217eabb89d9bd979
hasanalay <hsn_aly@hotmail.com>
2023-09-03 22:14:46 +0300

    feat: add homework 1 to src

--- Homework1.playground/Contents.swift
+++ Homework1.playground/Contents.swift
@@ -1,4 +1,96 @@
 import UIKit
 
-let odemeTarihi = "01/02/2023"
+let sehir = "İzmir"
+let ulke = "Türkiye"
+let telefon = 5055055050
+let postaKodu = 35870
+let mail = "hsn_aly@hotmail.com"
+let meslek = "Bilgisayar Mühendisi"
+let stokMiktari = 101
+let musteriAdi = "Hasan"
+let bakiye = 4999.50
+let dogumGunu = "01/01/1999"
+let maas = 19999
+let medeniDurum = "Bekar"
+let urunYorum = "Bu bir ürün yorumudur."
+let odemeTarihi = "03/09/2023"
+let odeme = 259.90
+let siparisAdeti = 4
+let arabaModeli = "Insignia"
+let kitapAdi = "İnsanlık 2.0"
+let yayinlanmaTarihi = "13/04/2014"
+let indirimMiktari = 25
+let odaSayisi = 3
+let enlem = "39° 54' 20\""
+let boylam = "29° 05′ 70\""
+let urunAdi = "Apple Magic Mouse 2"
+let yemekFiyati = 359.50
+let marka = "Apple"
+let muzikAdi = "All nightmare long"
+let videoSuresi = "17:46"
+let urunPuani = 4.75
+let resimAdi = "Screnshoot_1.png"
+let dosyaFormati = ".png"
+let renk = "Mavi"
+let renkKodu = "#0000ff"
+let telefonModeli = "iPhone 14 Pro"
+let ekranBoyutu = "6.1\""
+let agirlik = "250.5 gram"
+let ulusalGun = "29 Ekim"
+let tatilGunu = "29/10/2023"
+let rezervasyonTarihi = "01/01/2024"
+let sokakAdi = "Bekir Sakarya"
+let otobusHatti = 715
+let kalanDakika = 1456.50
+let takipKodu = "ghv1093049503"
+let kuponSuresi = "3 gün"
+let kuponKodu = "swiftogreniyorum15"
+let faturaAdresi = "Bu bir adrestir"
+
+print("Şehir: \(sehir)")
+print("Ülke: \(ulke)")
+print("Telefon: \(telefon)")
+print("Posta Kodu: \(postaKodu)")
+print("E-posta: \(mail)")
+print("Meslek: \(meslek)")
+print("Stok Miktarı: \(stokMiktari)")
+print("Müşteri Adı: \(musteriAdi)")
+print("Bakiye: \(bakiye)")
+print("Doğum Günü: \(dogumGunu)")
+print("Maaş: \(maas)")
+print("Medeni Durum: \(medeniDurum)")
+print("Ürün Yorumu: \(urunYorum)")
+print("Ödeme Tarihi: \(odemeTarihi)")
+print("Ödeme: \(odeme)")
+print("Sipariş Adeti: \(siparisAdeti)")
+print("Araba Modeli: \(arabaModeli)")
+print("Kitap Adı: \(kitapAdi)")
+print("Yayınlanma Tarihi: \(yayinlanmaTarihi)")
+print("İndirim Miktarı: \(indirimMiktari)")
+print("Oda Sayısı: \(odaSayisi)")
+print("Enlem: \(enlem)")
+print("Boylam: \(boylam)")
+print("Ürün Adı: \(urunAdi)")
+print("Yemek Fiyatı: \(yemekFiyati)")
+print("Marka: \(marka)")
+print("Müzik Adı: \(muzikAdi)")
+print("Video Süresi: \(videoSuresi)")
+print("Ürün Puanı: \(urunPuani)")
+print("Resim Adı: \(resimAdi)")
+print("Dosya Formatı: \(dosyaFormati)")
+print("Renk: \(renk)")
+print("Renk Kodu: \(renkKodu)")
+print("Telefon Modeli: \(telefonModeli)")
+print("Ekran Boyutu: \(ekranBoyutu)")
+print("Ağırlık: \(agirlik)")
+print("Ulusal Gün: \(ulusalGun)")
+print("Tatil Günü: \(tatilGunu)")
+print("Rezervasyon Tarihi: \(rezervasyonTarihi)")
+print("Sokak Adı: \(sokakAdi)")
+print("Otobüs Hattı: \(otobusHatti)")
+print("Kalan Dakika: \(kalanDakika)")
+print("Takip Kodu: \(takipKodu)")
+print("Kupon Süresi: \(kuponSuresi)")
+print("Kupon Kodu: \(kuponKodu)")
+print("Fatura Adresi: \(faturaAdresi)")
 
